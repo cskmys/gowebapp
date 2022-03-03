785168723811ad0dcf71628f8a4ba2456578044d
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 19:00:31 +0100

    replacing gorilla servemux by httprouter servemux

--- main.go
+++ main.go
@@ -2,40 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my Awesome site!!!</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	fmt.Fprint(w, "To get in touch, send me an email at <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQ Page</h1>")
+	fmt.Fprint(w, "<h1>FAQ Page...</h1>")
 }
 
 func custom404(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>Can't find the page you are looking for</h1><p>email me if you keep seeing this</p>")
+	fmt.Fprint(w, "<h1>Cannot find the page you are looking for</h1><p>email me if you keep seeing this</p>")
 }
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", home)
-	router.HandleFunc("/contact", contact)
-	router.HandleFunc("/faq", faq)
-	router.NotFoundHandler = http.HandlerFunc(custom404) // turning "custom404" into "http.Handler" requires "http.HandlerFunc"
-	// "http.Handler" is an interface that expects implementation of "ServerHTTP" and "http.HandlerFunc" implements "ServeHTTP" by calling the function passed to it
-	// the function passed to "http.HandlerFunc" will have "(w http.ResponseWriter, r *http.Request)" as parameters
-	// hence, "http.HandlerFunc" is an adaptor that converts regular functions with "(w http.ResponseWriter, r *http.Request)" parameters into "http.Handler"
-
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact) // here "/contact" and "/contact/" get routed to same function "contact"
+	router.GET("/faq", faq)
+	router.NotFound = http.HandlerFunc(custom404)
 	http.ListenAndServe(":3000", router)
 }
