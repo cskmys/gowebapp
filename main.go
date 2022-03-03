47a7b9048f042a51e5396091edc442b40a57c223
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 16:59:07 +0100

    using 3rd party servemux

--- main.go
+++ main.go
@@ -1,42 +1,26 @@
+// here we replace the default servemux with a 3rd party servemux
+
+// set "GOPATH" to somewhere else except where this file is
+// create a new "go.mod" file by doing "go mod init"
+// to install this "httprouter" package do "go get github.com/julienschmidt/httprouter"
+
 package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) { // ":name" regex matching is done with URL and the name field is available in "ps"
+	// with default servemux we had to manually do the regex matching to extract name from url, here it is automatically done and available in "ps"
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
-	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" { // to make sure that "/contact" and "/contact/" gets served the same page
-		fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>Can't find the page you are looking for</h1><p>email me if you keep seeing this</p>")
-	}
+	fmt.Fprintf(w, "<h1>Welcome %s!</h1>", ps.ByName("name"))
 }
 
 func main() {
-	mux := &http.ServeMux{} // creating "mux" from default servemux
-	mux.HandleFunc("abc/", handlerFunc)
-	// if pattern was "/contact" then exact match "localhost:3000/contact" is expected to call "handlerFunc"
-	// in this case, if there was no exact match d efault 404 page of servemux is returned
-	// if pattern was "/contact/" then any URL that starts with "localhost:3000/contact" followed by "/" or "/<something>" gets routed to "handlerFunc"
-	// hence, if there was no match 404 page written inside "handlerFunc" is returned
-	// therefore, when "/" is used as a pattern, every request gets routed to "handlerFunc"
-	/*
-		// we can have multiple routing functions
-		mux.HandleFunc("/contact/", handlerFunc2)
-		mux.HandleFunc("/contact/me/", handlerFunc3)
-		// now all URL that starts with "localhost:3000/contact/me" followed by "/" or  "/<something>" gets routed to "handlerFunc3"
-		// all URL that starts with "localhost:3000/contact" followed by "/" or  "/<something>"(where <something> is not "me" or "me/" or "me/<something_else>") gets routed to "handlerFunc2"
-		// all URL that starts with "localhost:3000" followed by "/" or  "/<something>"(where <something> is not "contact" or "contact/" or "contact/<something_else>") gets routed to "handlerFunc"
-		// hence, the longest pattern gets matched first
-	*/
-	// as the pattern is "/", in the browser you can use the ip alias of the localhost i.e. 127.0.0.1 along with port no as "127.0.0.1:3000" to talk to server
-	// if pattern was "localhost/", then in browser you cannot use the localhost alias, you'll have to do "localhost:3000"
-	// however if pattern was "abc/", then in browser using "abc:3000" will lead to browser's 404(not server's 404) meaning browser is not able to talk to server
-	// don't know why, maybe you'll need to register this application with OS or browser, so that browser will look for it to redirect port 3000 communications
-	http.ListenAndServe(":3000", mux) // now we explicitly say what servemux should be used
+	mux := httprouter.New()
+	mux.GET("/users/:name", Hello) // now, for all paths "localhost:3000/users/<name>", "Hello" gets called
+
+	http.ListenAndServe(":3000", mux)
 }
