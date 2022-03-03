22eff87417c705ebc396cb750ae6182617c41be3
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 11:57:39 +0100

    printing url path on the browser(just to see what it is)

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, r.URL.Path) // just to see the URL path
 	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
 	fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
