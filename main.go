c43ccaea99592d52a8ed977f0bf530bc93ee74ec
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 17:35:22 +0100

    bifurcated routing code into functions

--- main.go
+++ main.go
@@ -6,23 +6,20 @@ import (
 	"net/http"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
-	} else {
-		// this part will not run as Gorilla mux will throw its own 404 page in case of no url match
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>Can't find the page you are looking for</h1><p>email me if you keep seeing this</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", handleFunc) // for Gorilla mux "/" does not mean route all the URLs, it only means route "/" URL
-	router.HandleFunc("/contact", handleFunc)
+	router.HandleFunc("/", home)           // for Gorilla mux "/" does not mean route all the URLs, it only means route "/" URL
+	router.HandleFunc("/contact", contact) // "/contact" means only "/contact" not "/contact/"
 	// for any URL other than "/" and "/contact" mux throws its own default 404 page(even "/contact/" will give 404 page)
 
 	http.ListenAndServe(":3000", router)
