b3176dd43328d2588a808d7c98a386d702248778
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 18:45:27 +0100

    adding custom 404 page

--- main.go
+++ main.go
@@ -21,11 +21,21 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>FAQ Page</h1>")
 }
 
+func custom404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Can't find the page you are looking for</h1><p>email me if you keep seeing this</p>")
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
 	router.HandleFunc("/faq", faq)
+	router.NotFoundHandler = http.HandlerFunc(custom404) // turning "custom404" into "http.Handler" requires "http.HandlerFunc"
+	// "http.Handler" is an interface that expects implementation of "ServerHTTP" and "http.HandlerFunc" implements "ServeHTTP" by calling the function passed to it
+	// the function passed to "http.HandlerFunc" will have "(w http.ResponseWriter, r *http.Request)" as parameters
+	// hence, "http.HandlerFunc" is an adaptor that converts regular functions with "(w http.ResponseWriter, r *http.Request)" parameters into "http.Handler"
 
 	http.ListenAndServe(":3000", router)
 }
