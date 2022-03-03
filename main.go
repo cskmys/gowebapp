406b75c8d45833d4bf3193df9679b0337b5ddd2f
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 18:03:13 +0100

    adding faq page

--- main.go
+++ main.go
@@ -16,10 +16,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ Page</h1>")
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/faq", faq)
 
 	http.ListenAndServe(":3000", router)
 }
