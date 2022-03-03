6fcf308ce5e31a332c64a2b2922e51c05acf1231
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 11:15:28 +0100

    cleanup comments

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-// build and run this code and in the web browser enter "localhost:3000" and you should see the string with formatting
-// to see the actual string, right click and select "View page source"
-
 package main
 
 import (
@@ -8,15 +5,11 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) { // function run anytime a request arives at the server
-	// "http.ResponseWriter" is where you write the response
-	// "http.Request" is request object where you have info regarding the request
-	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>") // the string output is html with heading tag "h1"
-	// giving the html string will display string with formatting
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc) // "/" is the path after the address
-	// when we go to the path, the "servemux" is going to execute the "handlerFunc" function
-	http.ListenAndServe(":3000", nil) // ":3000" stands for "localhost:3000" and "nil" as the routing function means use the default routing function
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
