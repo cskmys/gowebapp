5508374d71a7da93755ede19e026fa48d3317396
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-03 12:03:28 +0100

    serving html based on url

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, r.URL.Path) // just to see the URL path
-	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
-	fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	if r.URL.Path == "/" { // homepage URL "/"
+		fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
+	} else if r.URL.Path == "/contact" { // contact page URL "/contact"
+		fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	} // if the URL is incorrect, no page is served
+	// now we are serving html based on url accessed in other words we are adding pages
 }
 
 func main() {
