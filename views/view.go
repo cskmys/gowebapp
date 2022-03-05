5d90f1280e23689a4270d1629c0c812c9f7c5bae
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-05 21:27:58 +0100

    glob-ing layout directory

--- views/view.go
+++ views/view.go
@@ -1,6 +1,9 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
 
 type View struct {
 	Template *template.Template
@@ -8,7 +11,8 @@ type View struct {
 }
 
 func NewView(layout string, files ...string) *View {
-	files = append(files, "views/layouts/bootstrap.gohtml", "views/layouts/footer.gohtml", "views/layouts/navbar.gohtml")
+	files = append(files, layoutFiles()...) // append takes a variadic parameter as the 2nd argument,
+	// so we put "..." after "layoutFiles()" to unpack it's return which is a string slice and pass it as a variadic parameter
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -18,4 +22,12 @@ func NewView(layout string, files ...string) *View {
 		Template: t,
 		Layout:   layout,
 	}
+}
+
+func layoutFiles() []string {
+	layoutFiles, err := filepath.Glob("views/layouts/*.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	return layoutFiles
 }
