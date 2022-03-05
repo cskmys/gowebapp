da4fac4f0089cdfcaf2430f2f0d5aa421572f016
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-03-05 17:45:10 +0100

    created a bootstrap layout and used it

--- main.go
+++ main.go
@@ -11,14 +11,17 @@ var homeView, contactView, custom404View *views.View
 
 func home(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.Execute(w, nil); err != nil {
+	// all gohtml files and templates are compiled during "homeView" creation
+	// now doing "ExecuteTemplate" will execute "bootstrap" from "views/layout/bootstrap.gohtml" which will call "yield" template
+	// for "homeView", "yield" is present in "views/home.gohtml" which will in turn call "footer" present in "views/layout/footer.gohtml"
+	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.Execute(w, nil); err != nil {
+	if err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
@@ -26,15 +29,17 @@ func contact(w http.ResponseWriter, _ *http.Request) {
 func custom404(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	if err := custom404View.Template.Execute(w, nil); err != nil {
+	if err := custom404View.Template.ExecuteTemplate(w, custom404View.Layout, nil); err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
-	custom404View = views.NewView("views/custom404.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	// "bootstrap" template's file "/views/layouts/bootstrap.gohtml" is hard coded to compile inside "views.NewView" along with other template files
+	// hence, just using it by its name rather than the file name is not a problem
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	custom404View = views.NewView("bootstrap", "views/custom404.gohtml")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
--- views/view.go
+++ views/view.go
@@ -4,10 +4,11 @@ import "html/template"
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files, "views/layouts/bootstrap.gohtml", "views/layouts/footer.gohtml")
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -15,5 +16,6 @@ func NewView(files ...string) *View {
 	}
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
